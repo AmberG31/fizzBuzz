a0c6c16e3d5e52c9fdba15666d916f7f61019a6f
addauk <addauk@gmail.com>
2023-01-12 16:10:50 +0000

    tidied up tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -2,84 +2,37 @@ require 'fizzbuzz'
 
 describe "fizzbuzz" do
 
-  it "returns '1' when passed 1" do
+  it "returns a number when passed 1 a non fizzbuzz number" do
     expect(fizzbuzz(1)).to eq 1
-  end
-
-  it "returns '2' when passed 2" do
-    expect(fizzbuzz(2)).to eq 2
-  end
-
-  it "returns '4' when passed 4" do
-    expect(fizzbuzz(4)).to eq 4
-  end
-
-  it "returns '7' when passed 7" do
-    expect(fizzbuzz(7)).to eq 7
-  end
-
-  it "returns '8' when passed 8" do
-    expect(fizzbuzz(8)).to eq 8
-  end
-
-  it "returns '11' when passed 1" do
-    expect(fizzbuzz(11)).to eq 11
-  end
-
-  it "returns '13' when passed 13" do
-    expect(fizzbuzz(13)).to eq 13
-  end
-
-  it "returns '14' when passed 14" do
-    expect(fizzbuzz(14)).to eq 14
-  end
-
-  it "returns '16' when passed 16" do
-    expect(fizzbuzz(16)).to eq 16
-  end
-
-  it "returns '17' when passed 17" do
     expect(fizzbuzz(17)).to eq 17
+    expect(fizzbuzz(22)).to eq 22
+    expect(fizzbuzz(28)).to eq 28
+    expect(fizzbuzz(47)).to eq 47
+    expect(fizzbuzz(98)).to eq 98
   end
 
-  it "returns '19' when passed 19" do
-    expect(fizzbuzz(19)).to eq 19
-  end
-
-  it "returns 'fizz' when passed 3" do
+  it "returns 'fizz' when passed a number divisible by only 3" do
     expect(fizzbuzz(3)).to eq "fizz"
-  end
-
-  it "returns 'fizz' when passed 6" do
     expect(fizzbuzz(6)).to eq "fizz"
-  end
-
-  it "returns 'fizz' when passed 9" do
     expect(fizzbuzz(9)).to eq "fizz"
-  end
-
-  it "returns 'fizz' when passed 12" do
     expect(fizzbuzz(12)).to eq "fizz"
-  end
-
-  it "returns 'fizz' when passed 18" do
     expect(fizzbuzz(18)).to eq "fizz"
+    expect(fizzbuzz(63)).to eq "fizz"
+    expect(fizzbuzz(99)).to eq "fizz"
   end
 
-  it "returns 'buzz' when passed 5" do
+  it "returns 'buzz' when passed numbers divsible by 5" do
     expect(fizzbuzz(5)).to eq "buzz"
-  end
-
-  it "returns 'buzz' when passed 10" do
     expect(fizzbuzz(10)).to eq "buzz"
-  end
-
-  it "returns 'buzz' when passed 20" do
     expect(fizzbuzz(20)).to eq "buzz"
+    expect(fizzbuzz(100)).to eq "buzz"
   end
 
-  it "returns 'fizzbuzz' when passed 15" do
+  it "returns 'fizzbuzz' when passed a number divisible by 15" do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
+    expect(fizzbuzz(45)).to eq "fizzbuzz"
+    expect(fizzbuzz(30)).to eq "fizzbuzz"
+    expect(fizzbuzz(90)).to eq "fizzbuzz"
   end
 end
 
